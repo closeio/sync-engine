c2f675cba0bf3ec0130fd7be33baa31e49e00b2b
Joe Kemp <joe+github@close.io>
2019-04-26 07:02:22 -0400

    Fix encoding (#39)

--- inbox/crispin.py
+++ inbox/crispin.py
@@ -593,7 +593,7 @@ class CrispinClient(object):
             'inbox': 'inbox',
             'drafts': 'drafts',
             'draft': 'drafts',
-            u'Entwürfe': 'drafts',
+            u'entw\xfcrfe': 'drafts',
             'junk': 'spam',
             'spam': 'spam',
             'archive': 'archive',

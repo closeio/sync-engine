0fc51ecc4a9c5f3c3e9b31c115840177f4cd9474
Thomas Steinacher <tom@eggdrop.ch>
2020-09-22 15:05:21 +0200

    Migration

---
+++ migrations/versions/253_drop_require_ssl.py
@@ -0,0 +1,32 @@
+"""drop ssl_required field
+
+Revision ID: 52783469ee6c
+Revises: 32df3d8ff73e
+Create Date: 2020-09-22 13:03:12.879960
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "52783469ee6c"
+down_revision = "32df3d8ff73e"
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import mysql
+
+
+def upgrade():
+    op.drop_column("genericaccount", "ssl_required")
+
+
+def downgrade():
+    op.add_column(
+        "genericaccount",
+        sa.Column(
+            "ssl_required",
+            mysql.TINYINT(display_width=1),
+            autoincrement=False,
+            nullable=True,
+            server_default="1",
+        ),
+    )

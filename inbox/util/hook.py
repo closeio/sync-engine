c65b9c969616f72cfe576ac10f2ea3d60e663de8
Squeaky <squeaky_pl@gmx.com>
2021-09-22 19:20:05 +0200

    black

--- inbox/util/hook.py
+++ inbox/util/hook.py
@@ -1,4 +1,5 @@
 import importlib
+import os
 
 import gevent.threading
 from nylas.logging import get_logger
@@ -10,12 +11,15 @@ _already_run = set()
 
 
 def run_once(hookspec):
+    if not hookspec:
+        return
+
     with _lock:
         if hookspec in _already_run:
             log.info("Not running hook {}, it was already run".format(hookspec))
             return
 
-        module_name, function_name = hookspec.rsplit('.', 1)
+        module_name, function_name = hookspec.rsplit(".", 1)
         module = importlib.import_module(module_name)
         function = getattr(module, function_name)
 
@@ -24,3 +28,7 @@ def run_once(hookspec):
         function()
 
         _already_run.add(hookspec)
+
+
+def run_startup():
+    return run_once(os.environ.get("SYNC_ENGINE_STARTUP_HOOK"))
--- tests/util/test_hook.py
+++ tests/util/test_hook.py
@@ -2,6 +2,7 @@ import mock
 
 from inbox.util.hook import run_once
 
+
 def hook():
     pass
 
@@ -10,22 +11,33 @@ def another_hook():
     pass
 
 
+def test_empty():
+    with mock.patch("inbox.util.hook.log") as log_mock:
+        run_once("")
+        run_once(None)
+
+    assert log_mock.info.call_count == 0
+
+
 def test_run_once():
     with mock.patch("inbox.util.hook.log") as log_mock:
         run_once("tests.util.test_hook.hook")
 
     assert log_mock.info.call_count == 1
-    assert log_mock.info.call_args == (('Running hook tests.util.test_hook.hook',),)
+    assert log_mock.info.call_args == (("Running hook tests.util.test_hook.hook",),)
 
     with mock.patch("inbox.util.hook.log") as log_mock:
         run_once("tests.util.test_hook.hook")
 
     assert log_mock.info.call_count == 1
-    assert log_mock.info.call_args == (('Not running hook tests.util.test_hook.hook, it was already run',),)
+    assert log_mock.info.call_args == (
+        ("Not running hook tests.util.test_hook.hook, it was already run",),
+    )
 
     with mock.patch("inbox.util.hook.log") as log_mock:
         run_once("tests.util.test_hook.another_hook")
 
     assert log_mock.info.call_count == 1
-    assert log_mock.info.call_args == (('Running hook tests.util.test_hook.another_hook',),)
-
+    assert log_mock.info.call_args == (
+        ("Running hook tests.util.test_hook.another_hook",),
+    )

5bc811a93ea853c8f2e3c2feb278de0ec61d1b2c
Paweł Piotr Przeradowski <showerproof86@gmail.com>
2021-09-30 16:13:33 +0200

    Remove superfluous tests.search module (#71)

--- tests/search/conftest.py
+++
@@ -1,4 +0,0 @@
-from tests.api.base import api_client
-from tests.util.base import absolute_path, config, db, default_namespace
-
-__all__ = ["config", "db", "absolute_path", "default_namespace", "api_client"]

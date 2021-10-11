6faf7ae8cb6f85c61079fb929cdda1e79f661725
Squeaky <squeaky_pl@gmx.com>
2021-10-11 15:06:44 +0200

    black

--- bin/delete-marked-accounts.py
+++ bin/delete-marked-accounts.py
@@ -29,16 +29,15 @@ log = get_logger()
 
 
 @click.command()
-@click.option('--throttle', is_flag=True)
-@click.option('--dry-run', is_flag=True)
+@click.option("--throttle", is_flag=True)
+@click.option("--dry-run", is_flag=True)
 def run(throttle, dry_run):
     maybe_enable_rollbar()
 
     pool = []
 
-    for host in config['DATABASE_HOSTS']:
-        log.info("Spawning delete process for host",
-                 host=host['HOSTNAME'])
+    for host in config["DATABASE_HOSTS"]:
+        log.info("Spawning delete process for host", host=host["HOSTNAME"])
         pool.append(gevent.spawn(delete_account_data, host, throttle, dry_run))
 
     gevent.joinall(pool)
@@ -46,13 +45,13 @@ def run(throttle, dry_run):
 
 def delete_account_data(host, throttle, dry_run):
     while True:
-        for shard in host['SHARDS']:
+        for shard in host["SHARDS"]:
             # Ensure shard is explicitly not marked as disabled
-            if 'DISABLED' in shard and not shard['DISABLED']:
-                namespace_ids = get_accounts_to_delete(shard['ID'])
+            if "DISABLED" in shard and not shard["DISABLED"]:
+                namespace_ids = get_accounts_to_delete(shard["ID"])
                 batch_delete_namespaces(namespace_ids, throttle, dry_run)
         gevent.sleep(600)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()

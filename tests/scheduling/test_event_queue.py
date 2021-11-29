798f05922be1ffe3b0c93d691a36703f47bafa49
Paweł Piotr Przeradowski <showerproof86@gmail.com>
2021-11-29 10:56:04 +0100

    Fix queue_name returned as bytes on Python 3 (#251)

---
+++ tests/scheduling/test_event_queue.py
@@ -0,0 +1,37 @@
+from inbox.scheduling.event_queue import EventQueue, EventQueueGroup
+
+
+def test_event_queue():
+    queue = EventQueue("name")
+    sent_event = {"event": "test"}
+    queue.send_event(sent_event)
+    received_event = queue.receive_event()
+
+    assert received_event.pop("queue_name") == queue.queue_name
+    assert received_event == sent_event
+
+
+def test_event_queue_empty():
+    queue = EventQueue("name")
+    assert queue.receive_event(1) is None
+
+
+def test_event_queue_group():
+    queue1 = EventQueue("name1")
+    queue2 = EventQueue("name2")
+
+    sent_event_1 = {"event": "test1"}
+    queue1.send_event(sent_event_1)
+
+    sent_event_2 = {"event": "test2"}
+    queue2.send_event(sent_event_2)
+
+    queue_group = EventQueueGroup([queue1, queue2])
+
+    received_event_1 = queue_group.receive_event()
+    assert received_event_1.pop("queue_name") == queue1.queue_name
+    assert received_event_1 == sent_event_1
+
+    received_event_2 = queue_group.receive_event()
+    assert received_event_2.pop("queue_name") == queue2.queue_name
+    assert received_event_2 == sent_event_2

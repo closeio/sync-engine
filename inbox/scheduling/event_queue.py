3116e1404157487ea71891fb92a3ba4f142210ec
Paweł Piotr Przeradowski <showerproof86@gmail.com>
2021-11-29 16:05:44 +0100

    Fix the case where queue_name is already string because it did not arrive from Redis. (#268)

--- inbox/scheduling/event_queue.py
+++ inbox/scheduling/event_queue.py
@@ -37,19 +37,38 @@ class EventQueue(object):
         self.queue_name = queue_name
 
     def receive_event(self, timeout=0):
-        # type: (int) -> Dict[str, Any]
-        result = None  # type: Optional[Union[bytes, Tuple[bytes, bytes]]]
-        if timeout is None:
-            result = self.redis.lpop(self.queue_name)
-        else:
-            result = self.redis.blpop([self.queue_name], timeout=timeout)
+        # type: (Optional[int]) -> Optional[Dict[str, Any]]
+        """
+        Receive single event from the queue.
 
-        if result is None:
-            return None
-        queue_name, event_data = (
-            (self.queue_name, result) if timeout is None else result
-        )
-        queue_name = queue_name.decode("utf-8")
+        * When timeout is set to 0:
+            Block infinitely until receiving an event and return event dictionary.
+        * When timeout is positive integer:
+            Return None if the queue was still empty after timeout seconds.
+            Return event dictionary if the queue was not empty.
+        * When timeout is set to None:
+            Return None immediately if the queue was empty.
+            Return event dictionary if the queue was not empty.
+        """
+        assert self.redis
+
+        if timeout is None:
+            lpop_result = self.redis.lpop(self.queue_name)  # type: Optional[bytes]
+            if lpop_result is None:
+                return None
+
+            queue_name = self.queue_name  # type: str
+            event_data = lpop_result
+        else:
+            blpop_result = self.redis.blpop(
+                [self.queue_name], timeout=timeout
+            )  # type: Optional[Tuple[bytes, bytes]]
+            if blpop_result is None:
+                return None
+
+            blpop_queue_name, event_data = blpop_result
+            queue_name = blpop_queue_name.decode("utf-8")
+
         try:
             event = json.loads(event_data)
             event["queue_name"] = queue_name
@@ -62,6 +81,8 @@ class EventQueue(object):
 
     def send_event(self, event_data):
         # type: (Dict[str, Any]) -> None
+        assert self.redis
+
         event_data.pop("queue_name", None)
         self.redis.rpush(self.queue_name, json.dumps(event_data))
 
@@ -77,14 +98,16 @@ class EventQueueGroup(object):
             self.redis = self.queues[0].redis
 
     def receive_event(self, timeout=0):
-        # type: (int) -> Dict[str, Any]
+        # type: (int) -> Optional[Dict[str, Any]]
+        assert self.redis
+
         result = self.redis.blpop(
             [q.queue_name for q in self.queues], timeout=timeout
         )  # type: Optional[Tuple[bytes, bytes]]
         if result is None:
             return None
-        queue_name, event_data = result
-        queue_name = queue_name.decode("utf-8")
+        blpop_queue_name, event_data = result
+        queue_name = blpop_queue_name.decode("utf-8")
         try:
             event = json.loads(event_data)
             event["queue_name"] = queue_name

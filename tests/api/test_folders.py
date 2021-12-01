ef84bc6e8e3e87a04dcda6311f09f833c7e225cb
Squeaky <squeaky_pl@gmx.com>
2021-12-01 17:46:37 +0100

    OK, let's do this

--- tests/api/test_folders.py
+++ tests/api/test_folders.py
@@ -71,6 +71,7 @@ def test_folder_name_translation(
 
     syncback._process_log()
     syncback._restart_workers()
+    gevent.sleep(0.1)
     while not syncback.task_queue.empty():
         print("Sleep")
         gevent.sleep(0.1)

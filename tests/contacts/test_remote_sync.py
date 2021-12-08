cc515fff5e1f328619342bf0184a9fb94b93f236
Squeaky <squeaky_pl@gmx.com>
2021-12-08 13:23:20 +0100

    increase timeout

--- tests/contacts/test_remote_sync.py
+++ tests/contacts/test_remote_sync.py
@@ -97,7 +97,7 @@ def test_auth_error_handling(contact_sync, default_account, db):
     db.session.commit()
 
     contact_sync.start()
-    contact_sync.join(timeout=10)
+    contact_sync.join(timeout=15)
     success = contact_sync.successful()
     if not success:
         contact_sync.kill()

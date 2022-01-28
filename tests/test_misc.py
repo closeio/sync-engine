3c87a1c488ca1e22e54a47726756fbf1593d04b8
Andrés Barreiro <andres.bga@gmail.com>
2022-01-28 16:20:44 +0100

    Add basic hash_token test

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,4 +1,7 @@
+import pytest
 import setproctitle
+
+from inbox.models.backends.oauth import hash_token
 
 
 def test_setproctitle_works():
@@ -8,3 +11,25 @@ def test_setproctitle_works():
     assert setproctitle.getproctitle() == test_setproctitle_works.__name__
 
     setproctitle.setproctitle(original_proctitle)
+
+
+@pytest.mark.parametrize(
+    "prefix,token,expected",
+    [
+        (None, None, None),
+        ("prefix", None, None),
+        (
+            None,
+            "secret_token",
+            "26d141c9a7204906121db95e892d2c8b500c3db440c06d3eaa6715f0cbf8763f",
+        ),
+        (
+            "prefix",
+            "secret_token",
+            "c848fa75ec9d92cf38b68f5fd3db6ae1941d3bebebdb5c353878a656aa4dfa4a",
+        ),
+    ],
+)
+def test_hash_token(token, prefix, expected):
+    result = hash_token(token, prefix)
+    assert result == expected

858a58c0d0742b6b94ef4effffd7f8b7cc0a1767
Paweł Piotr Przeradowski <showerproof86@gmail.com>
2022-10-19 10:09:13 +0200

    Microsoft Graph Client (#371)

---
+++ inbox/events/microsoft_graph_client.py
@@ -0,0 +1,415 @@
+import contextlib
+import datetime
+import enum
+import time
+from typing import Any, Callable, Dict, Iterable, Optional
+
+import pytz
+import requests
+
+BASE_URL = "https://graph.microsoft.com/v1.0"
+
+
+class ChangeType(enum.Enum):
+    UPDATED = "updated"
+    CREATED = "created"
+    DELETED = "deleted"
+
+
+class MicrosoftGraphClientException(Exception):
+    def __init__(self, response: requests.Response):
+        """
+        Arguments:
+            response: The reponse that caused exception
+        """
+        args = []
+        with contextlib.suppress(Exception):
+            error = response.json()["error"]
+            code = error["code"]
+            args.append(code)
+            message = error["message"]
+            args.append(message)
+
+        super().__init__(*args)
+        self.response = response
+
+
+def format_datetime(dt: datetime.datetime) -> str:
+    """
+    Format UTC datetime in format accepted by Microsoft Graph.
+
+    Arguments:
+        dt: the datetime
+
+    Returns:
+        Formatted datetime e.g. 2022-10-17T15:46:36.335288Z
+    """
+    assert dt.tzinfo == pytz.UTC
+
+    return dt.replace(tzinfo=None).isoformat() + "Z"
+
+
+class MicrosoftGraphClient:
+    """
+    Provide basic operations to read Outlook calendar
+    over Microsoft Graph API.
+    """
+
+    def __init__(self, get_token: Callable[[], str]):
+        """
+        Arguments:
+            get_token: Function that returns user token
+        """
+        self._get_token = get_token
+        # Session lets us use HTTP connection pooling
+        # lowering the number of TCP connections needed
+        # to make many requests
+        self._session = requests.Session()
+
+    def request(
+        self,
+        method: str,
+        resource_url: str,
+        *,
+        params: Optional[Dict[str, str]] = None,
+        json: Optional[Dict[str, str]] = None,
+        max_retries=10,
+        timeout=10,
+    ) -> Dict[str, Any]:
+        """
+        Perform request.
+
+        Automatically recover from 429 Too Many Requests.
+
+        Arguments:
+            method: HTTP method
+            resource_url: Either full URL or part after version e.g /me/calendars
+            params: GET parameters
+            json: JSON to POST or PATCH
+            max_retries: The number of maximum retires for 429
+            timeout: HTTP request timeout in seconds
+        """
+        if not resource_url.startswith(BASE_URL):
+            assert resource_url.startswith("/")
+            resource_url = BASE_URL + resource_url
+
+        headers = {"Authorization": "Bearer " + self._get_token()}
+
+        retry = 0
+        while retry < max_retries:
+            response = self._session.request(
+                method,
+                resource_url,
+                params=params,
+                headers=headers,
+                json=json,
+                timeout=timeout,
+            )
+            if response.status_code == 429:
+                sleep_seconds = int(response.headers.get("Retry-After", 5))
+                time.sleep(sleep_seconds)
+                retry += 1
+                continue
+            break
+
+        try:
+            response.raise_for_status()
+        except requests.HTTPError as e:
+            raise MicrosoftGraphClientException(response) from e
+
+        if not response.text:
+            # Some DELETE operations return empty body
+            return {}
+        else:
+            return response.json()
+
+    def _iter(
+        self, initial_url: str, *, params: Optional[Dict[str, str]] = None
+    ) -> Iterable[Dict[str, Any]]:
+        """
+        Lazily iterate over paged resources.
+
+        Arguments:
+            initial_url: The initial resource url
+            params: GET params
+        """
+        next_url: Optional[str] = initial_url
+        while next_url:
+            response = self.request("GET", next_url, params=params)
+            yield from response["value"]
+            next_url = response.get("@odata.nextLink")
+            # @odata.nextLink will already incorporate params
+            params = None
+
+    def iter_calendars(self) -> Iterable[Dict[str, Any]]:
+        """
+        Lazily iterate user calendars.
+
+        https://learn.microsoft.com/en-us/graph/api/user-list-calendars
+
+        Returns:
+            Iterable of calendars.
+            https://learn.microsoft.com/en-us/graph/api/resources/calendar
+        """
+        # TODO: Figure out the top limit we can use on this endpoint
+        yield from self._iter("/me/calendars")
+
+    def get_calendar(self, calendar_id: str) -> Dict[str, Any]:
+        """
+        Get a single calendar.
+
+        https://learn.microsoft.com/en-us/graph/api/calendar-get
+
+        Arguments:
+            calendar_id: The calendar id
+
+        Returns:
+            The calendar.
+            https://learn.microsoft.com/en-us/graph/api/resources/calendar
+        """
+        return self.request("GET", f"/me/calendars/{calendar_id}")
+
+    def iter_events(
+        self, calendar_id: str, *, modified_after: Optional[datetime.datetime] = None
+    ) -> Iterable[Dict[str, Any]]:
+        """
+        Lazily iterate events in a calendar.
+
+        https://learn.microsoft.com/en-us/graph/api/user-list-events
+
+        Arguments:
+            calendar_id: The calendar id
+            modified_after: If specified only return events given datetime
+
+        Returns:
+            Iterable of events.
+            https://learn.microsoft.com/en-us/graph/api/resources/event
+        """
+        if modified_after:
+            assert modified_after.tzinfo == pytz.UTC
+            params = {
+                "$filter": f"lastModifiedDateTime gt {format_datetime(modified_after)}"
+            }
+        else:
+            params = None
+        # TODO: Figure out the top limit we can use on this endpoint
+        yield from self._iter(f"/me/calendars/{calendar_id}/events", params=params)
+
+    def get_event(self, event_id: str) -> Dict[str, Any]:
+        """
+        Get a single event.
+
+        https://learn.microsoft.com/en-us/graph/api/event-get
+
+        Arguments:
+            event_id: The event id
+
+        Returns:
+            The event.
+            https://learn.microsoft.com/en-us/graph/api/resources/event
+        """
+        return self.request("GET", f"/me/events/{event_id}")
+
+    def iter_event_instances(
+        self, event_id: str, *, start: datetime.datetime, end: datetime.datetime
+    ) -> Iterable[Dict[str, Any]]:
+        """
+        Lazily expand series master instances.
+
+        https://learn.microsoft.com/en-us/graph/api/event-list-instances
+
+        Arguments:
+            event_id: The event id
+            start: the start of the period
+            end: the end of the period
+
+        Returns:
+            Iterable of event instances.
+            https://learn.microsoft.com/en-us/graph/api/resources/event
+        """
+        assert start.tzinfo == pytz.UTC
+        assert end.tzinfo == pytz.UTC
+        assert end >= start
+        yield from self._iter(
+            f"/me/events/{event_id}/instances",
+            params={
+                "startDateTime": format_datetime(start),
+                "endDateTime": format_datetime(end),
+                # The default amount of instances per page is 10,
+                # as we want to do the least
+                # amount of requests possible we raise it to 500.
+                "top": "500",
+            },
+        )
+
+    def iter_subscriptions(self) -> Iterable[Dict[str, Any]]:
+        """
+        Lazily iterate subscriptions.
+
+        https://learn.microsoft.com/en-us/graph/api/subscription-list
+
+        Returns:
+            Iterable of subscriptions.
+            https://learn.microsoft.com/en-us/graph/api/resources/subscription
+        """
+        yield from self._iter("/subscriptions")
+
+    def get_subscription(self, subscription_id: str) -> Dict[str, Any]:
+        """
+        Get a single subscription.
+
+        https://learn.microsoft.com/en-us/graph/api/subscription-get
+
+        Arguments:
+            subscription_id: The subscription id
+
+        Returns:
+            The subscription.
+            https://learn.microsoft.com/en-us/graph/api/resources/subscription
+        """
+        return self.request("GET", f"/subscriptions/{subscription_id}")
+
+    def subscribe(
+        self,
+        *,
+        resource_url: str,
+        change_types: Iterable[ChangeType],
+        webhook_url: str,
+        secret: str,
+        expiration: Optional[datetime.datetime] = None,
+    ) -> Dict[str, Any]:
+        """
+        Subscribe to resource changes.
+
+        https://learn.microsoft.com/en-us/graph/api/subscription-post-subscriptions
+
+        Arguments:
+            resource_url: The resource URL we want to subscribe to
+            change_types: Types of changes we want to listen to
+            webhook_url: The webhook URL that should receive changes
+            secret: Secret that can be used to verify authenticity of
+                date arriving on a webhook
+            expiration: The date that subscription will expire
+
+        Returns:
+            The subscription.
+            https://learn.microsoft.com/en-us/graph/api/resources/subscription
+        """
+
+        if resource_url.startswith(BASE_URL):
+            resource_url = resource_url[len(BASE_URL) :]
+
+        assert resource_url.startswith("/")
+
+        if not expiration:
+            # The maximum expiration for a webhook subscription
+            # is 4230 minutes, which is slightly less than 3 days.
+            expiration = datetime.datetime.now(tz=pytz.UTC) + datetime.timedelta(
+                minutes=4230
+            )
+        assert expiration.tzinfo == pytz.UTC
+
+        json = {
+            "changeType": ",".join(change_type.value for change_type in change_types),
+            "notificationUrl": webhook_url,
+            "resource": resource_url,
+            "expirationDateTime": format_datetime(expiration),
+            "clientState": secret,
+        }
+
+        return self.request("POST", "/subscriptions", json=json)
+
+    def unsubscribe(self, subscription_id: str) -> Dict[str, Any]:
+        """
+        Unsubscribe from resource changes.
+
+        https://learn.microsoft.com/en-us/graph/api/subscription-delete
+
+        Arguments:
+            subscription_id: The subscription id
+
+        Returns:
+            Empty dict.
+        """
+        return self.request("DELETE", f"/subscriptions/{subscription_id}")
+
+    def renew_subscription(
+        self, subscription_id: str, expiration: Optional[datetime.datetime] = None
+    ) -> Dict[str, Any]:
+        """
+        Renew a subscription before it expires.
+
+        https://learn.microsoft.com/en-us/graph/api/subscription-update
+
+        Arguments:
+            subscription_id: The subscription id
+            expiration: The new date that subscription will expire at
+
+        Returns:
+            The subscription.
+            https://learn.microsoft.com/en-us/graph/api/resources/subscription
+        """
+        if not expiration:
+            expiration = datetime.datetime.now(tz=pytz.UTC) + datetime.timedelta(
+                minutes=4230
+            )
+        assert expiration.tzinfo == pytz.UTC
+
+        json = {
+            "expirationDateTime": format_datetime(expiration),
+        }
+
+        return self.request("PATCH", f"/subscriptions/{subscription_id}", json=json)
+
+    def subscribe_to_calendar_changes(
+        self, *, webhook_url: str, secret: str
+    ) -> Dict[str, Any]:
+        """
+        Subscribe to calendar changes.
+
+        https://learn.microsoft.com/en-us/graph/api/subscription-post-subscriptions
+
+        Arguments:
+            webhook_url: The webhook URL that should receive changes
+            secret: Secret that can be used to verify authenticity of
+                date arriving on a webhook
+
+        Returns:
+            The subscription.
+            https://learn.microsoft.com/en-us/graph/api/resources/subscription
+        """
+        return self.subscribe(
+            resource_url="/me/calendars",
+            # Quirk: subscribing to "created" on calendars raises an API error.
+            # Nonetheless calendar creations, updates and deletes are all delivered
+            # as updates to webhooks.
+            change_types=[ChangeType.UPDATED, ChangeType.DELETED],
+            webhook_url=webhook_url,
+            secret=secret,
+        )
+
+    def subscribe_to_event_changes(
+        self, calendar_id: str, *, webhook_url: str, secret: str
+    ) -> Dict[str, Any]:
+        """
+        Subscribe to event changes in a calendar.
+
+        https://learn.microsoft.com/en-us/graph/api/subscription-post-subscriptions
+
+        Arguments:
+            calendar_id: The calendar id
+            webhook_url: The webhook URL that should receive changes
+            secret: Secret that can be used to verify authenticity of
+                date arriving on a webhook
+
+        Returns:
+            The subscription.
+            https://learn.microsoft.com/en-us/graph/api/resources/subscription
+        """
+
+        return self.subscribe(
+            resource_url=f"/me/calendars/{calendar_id}/events",
+            change_types=[ChangeType.CREATED, ChangeType.UPDATED, ChangeType.DELETED],
+            webhook_url=webhook_url,
+            secret=secret,
+        )
---
+++ tests/events/test_microsoft_graph_client.py
@@ -0,0 +1,355 @@
+import datetime
+import json
+import unittest.mock
+
+import ciso8601
+import pytest
+import pytz
+import responses
+from responses.registries import OrderedRegistry
+
+from inbox.events.microsoft_graph_client import (
+    BASE_URL,
+    MicrosoftGraphClient,
+    MicrosoftGraphClientException,
+    format_datetime,
+)
+
+
+def test_format_datetime():
+    assert (
+        format_datetime(datetime.datetime(2022, 10, 1, 3, 4, 5, tzinfo=pytz.UTC))
+        == "2022-10-01T03:04:05Z"
+    )
+
+
+@pytest.fixture
+def client():
+    return MicrosoftGraphClient(lambda: "fake_token")
+
+
+calendars_json = {
+    "value": [
+        {
+            "id": "AAMkADdiYzg5OGRlLTY1MjktNDc2Ni05YmVkLWMxMzFlNTQ0MzU3YQBGAAAAAACi9RQWB-SNTZBuALM6KIOsBwBtf4g8yY_zTZgZh6x0X-50AAAAAAEGAABtf4g8yY_zTZgZh6x0X-50AAAAADafAAA=",
+            "name": "Calendar",
+        },
+        {
+            "id": "AAMkADdiYzg5OGRlLTY1MjktNDc2Ni05YmVkLWMxMzFlNTQ0MzU3YQBGAAAAAACi9RQWB-SNTZBuALM6KIOsBwBtf4g8yY_zTZgZh6x0X-50AAAAAAEGAABtf4g8yY_zTZgZh6x0X-50AAAAADagAAA=",
+            "name": "United States holidays",
+        },
+        {
+            "id": "AAMkADdiYzg5OGRlLTY1MjktNDc2Ni05YmVkLWMxMzFlNTQ0MzU3YQBGAAAAAACi9RQWB-SNTZBuALM6KIOsBwBtf4g8yY_zTZgZh6x0X-50AAAAAAEGAABtf4g8yY_zTZgZh6x0X-50AAAAADajAAA=",
+            "name": "Birthdays",
+        },
+        {
+            "id": "AAMkADdiYzg5OGRlLTY1MjktNDc2Ni05YmVkLWMxMzFlNTQ0MzU3YQBGAAAAAACi9RQWB-SNTZBuALM6KIOsBwBtf4g8yY_zTZgZh6x0X-50AAAAAAEGAABtf4g8yY_zTZgZh6x0X-50AAIM0_ZOAAA=",
+            "name": "Test",
+        },
+    ],
+}
+
+
+@responses.activate
+def test_request(client):
+    def request_callback(request):
+        assert request.method == "GET"
+        assert request.url == BASE_URL + "/me/calendars"
+        assert request.headers["Authorization"] == "Bearer fake_token"
+
+        return (200, {}, json.dumps(calendars_json))
+
+    responses.add_callback(
+        responses.GET,
+        BASE_URL + "/me/calendars",
+        callback=request_callback,
+        content_type="application/json",
+    )
+
+    list(client.iter_calendars())
+
+
+@responses.activate(registry=OrderedRegistry)
+def test_request_retry(client):
+    responses.get(BASE_URL + "/me/calendars", status=429, headers={"Retry-After": "12"})
+    responses.get(BASE_URL + "/me/calendars", status=429, headers={"Retry-After": "3"})
+    responses.get(BASE_URL + "/me/calendars", json=calendars_json)
+
+    with unittest.mock.patch("time.sleep") as sleep_mock:
+        list(client.iter_calendars())
+
+    ((args1, _), (args2, _)) = sleep_mock.call_args_list
+    assert args1 == (12,)
+    assert args2 == (3,)
+
+
+bad_id_json = {
+    "error": {
+        "code": "ErrorInvalidIdMalformed",
+        "message": "Id is malformed.",
+        "innerError": {
+            "date": "2022-10-18T11:42:54",
+            "request-id": "5a07348e-1150-42cc-a2a4-5a30064ccb65",
+            "client-request-id": "3d6261a4-3882-61c8-39d1-cd4878fdb503",
+        },
+    }
+}
+
+
+@responses.activate
+def test_request_exception(client):
+    responses.get(BASE_URL + "/me/calendars/bad_id", status=400, json=bad_id_json)
+
+    with pytest.raises(MicrosoftGraphClientException) as exc_info:
+        client.request("GET", "/me/calendars/bad_id")
+
+    assert exc_info.value.args == ("ErrorInvalidIdMalformed", "Id is malformed.")
+    assert exc_info.value.response.status_code == 400
+
+
+@responses.activate
+def test_iter_calendars(client):
+    responses.get(
+        BASE_URL + "/me/calendars", json=calendars_json,
+    )
+
+    calendars = client.iter_calendars()
+    assert {calendar["name"] for calendar in calendars} == {
+        "Calendar",
+        "Birthdays",
+        "United States holidays",
+        "Test",
+    }
+
+
+@responses.activate
+def test_get_calendar(client):
+    responses.get(
+        BASE_URL + f"/me/calendars/{calendars_json['value'][0]['id']}",
+        json=calendars_json["value"][0],
+    )
+
+    calendar = client.get_calendar(calendars_json["value"][0]["id"])
+    assert calendar["name"] == "Calendar"
+
+
+events_json = {
+    "value": [
+        {
+            "id": "AAMkADdiYzg5OGRlLTY1MjktNDc2Ni05YmVkLWMxMzFlNTQ0MzU3YQBGAAAAAACi9RQWB-SNTZBuALM6KIOsBwBtf4g8yY_zTZgZh6x0X-50AAAAAAENAABtf4g8yY_zTZgZh6x0X-50AAIDZtFgAAA=",
+            "lastModifiedDateTime": "2022-09-09T12:09:43.7205143Z",
+            "subject": "Business meeting",
+        },
+        {
+            "id": "AAMkADdiYzg5OGRlLTY1MjktNDc2Ni05YmVkLWMxMzFlNTQ0MzU3YQBGAAAAAACi9RQWB-SNTZBuALM6KIOsBwBtf4g8yY_zTZgZh6x0X-50AAAAAAENAABtf4g8yY_zTZgZh6x0X-50AAIDZtFaAAA=",
+            "lastModifiedDateTime": "2022-09-09T08:39:48.5690063Z",
+            "subject": "Contract negotations",
+        },
+        {
+            "id": "AAMkADdiYzg5OGRlLTY1MjktNDc2Ni05YmVkLWMxMzFlNTQ0MzU3YQBGAAAAAACi9RQWB-SNTZBuALM6KIOsBwBtf4g8yY_zTZgZh6x0X-50AAAAAAENAABtf4g8yY_zTZgZh6x0X-50AAIARKVOAAA=",
+            "lastModifiedDateTime": "2022-09-07T17:36:12.925491Z",
+            "subject": "Renewal agreement",
+        },
+        {
+            "id": "AAMkADdiYzg5OGRlLTY1MjktNDc2Ni05YmVkLWMxMzFlNTQ0MzU3YQBGAAAAAACi9RQWB-SNTZBuALM6KIOsBwBtf4g8yY_zTZgZh6x0X-50AAAAAAENAABtf4g8yY_zTZgZh6x0X-50AAIARKVNAAA=",
+            "lastModifiedDateTime": "2022-09-07T17:43:56.0246319Z",
+            "subject": "API questions",
+        },
+    ]
+}
+
+
+@responses.activate
+def test_iter_events(client):
+    responses.get(
+        BASE_URL + "/me/calendars/fake_calendar_id/events", json=events_json,
+    )
+
+    events = client.iter_events("fake_calendar_id")
+    assert {event["subject"] for event in events} == {
+        "Business meeting",
+        "Contract negotations",
+        "Renewal agreement",
+        "API questions",
+    }
+
+
+@responses.activate
+@pytest.mark.parametrize(
+    "modified_after,subjects",
+    [
+        (datetime.datetime(2022, 9, 9, 12, tzinfo=pytz.UTC), {"Business meeting"}),
+        (
+            datetime.datetime(2022, 9, 8, 12, tzinfo=pytz.UTC),
+            {"Business meeting", "Contract negotations"},
+        ),
+    ],
+)
+def test_iter_events_modified_after(client, modified_after, subjects):
+    def request_callback(request):
+        ((_, odata_filter),) = request.params.items()
+        _, _, modified_after = odata_filter.split()
+        modified_after = ciso8601.parse_datetime(modified_after)
+
+        events = [
+            event
+            for event in events_json["value"]
+            if ciso8601.parse_datetime(event["lastModifiedDateTime"]) > modified_after
+        ]
+
+        return (200, {}, json.dumps({"value": events}))
+
+    responses.add_callback(
+        responses.GET,
+        BASE_URL + "/me/calendars/fake_calendar_id/events",
+        callback=request_callback,
+        content_type="application/json",
+    )
+
+    events = client.iter_events("fake_calendar_id", modified_after=modified_after,)
+
+    assert {event["subject"] for event in events} == subjects
+
+
+@responses.activate
+def test_get_event(client):
+    responses.get(
+        BASE_URL + f"/me/events/{events_json['value'][0]['id']}",
+        json=events_json["value"][0],
+    )
+
+    event = client.get_event(events_json["value"][0]["id"])
+    assert event["subject"] == "Business meeting"
+
+
+event_instances_first_page = {
+    "@odata.nextLink": "https://graph.microsoft.com/v1.0/me/events/fake_event_id/instances?startDateTime=2022-10-17&endDateTime=2023-10-17&$skip=2",
+    "value": [
+        {
+            "id": "AAMkADdiYzg5OGRlLTY1MjktNDc2Ni05YmVkLWMxMzFlNTQ0MzU3YQFRAAgI2q-SiNUAAEYAAAAAovUUFgf0jU2QbgCzOiiDrAcAbX_IPMmPs02YGYesdF-_dAACG91UqgAAbX_IPMmPs02YGYesdF-_dAACG93T0AAAEA==",
+            "subject": "Instance 1",
+        },
+        {
+            "id": "AAMkADdiYzg5OGRlLTY1MjktNDc2Ni05YmVkLWMxMzFlNTQ0MzU3YQFRAAgI2rCbsz7AAEYAAAAAovUUFgf0jU2QbgCzOiiDrAcAbX_IPMmPs02YGYesdF-_dAACG91UqgAAbX_IPMmPs02YGYesdF-_dAACG93T0AAAEA==",
+            "subject": "Instance 2",
+        },
+    ],
+}
+
+event_instances_second_page = {
+    "value": [
+        {
+            "id": "AAMkADdiYzg5OGRlLTY1MjktNDc2Ni05YmVkLWMxMzFlNTQ0MzU3YQFRAAgI2rFk3aiAAEYAAAAAovUUFgf0jU2QbgCzOiiDrAcAbX_IPMmPs02YGYesdF-_dAACG91UqgAAbX_IPMmPs02YGYesdF-_dAACG93T0AAAEA==",
+            "subject": "Instance 3",
+        }
+    ]
+}
+
+
+@responses.activate(registry=OrderedRegistry)
+def test_iter_event_instances(client):
+    responses.get(
+        BASE_URL + "/me/events/fake_event_id/instances",
+        json=event_instances_first_page,
+    )
+
+    responses.get(
+        event_instances_first_page["@odata.nextLink"], json=event_instances_second_page
+    )
+
+    instances = client.iter_event_instances(
+        "fake_event_id",
+        start=datetime.datetime(2022, 10, 17, tzinfo=pytz.UTC),
+        end=datetime.datetime(2023, 10, 17, tzinfo=pytz.UTC),
+    )
+
+    assert {instance["subject"] for instance in instances} == {
+        "Instance 1",
+        "Instance 2",
+        "Instance 3",
+    }
+
+
+@responses.activate
+def test_subscribe_to_calendar_changes(client):
+    def request_callback(request):
+        return (200, {}, request.body)
+
+    responses.add_callback(
+        responses.POST,
+        BASE_URL + "/subscriptions",
+        callback=request_callback,
+        content_type="application/json",
+    )
+
+    subscription = client.subscribe_to_calendar_changes(
+        webhook_url="https://example.com", secret="s3cr3t"
+    )
+
+    assert subscription["resource"] == "/me/calendars"
+    assert subscription["changeType"] == "updated,deleted"
+    assert subscription["notificationUrl"] == "https://example.com"
+    assert subscription["clientState"] == "s3cr3t"
+
+
+@responses.activate
+def test_subscribe_to_event_changes(client):
+    def request_callback(request):
+        return (200, {}, request.body)
+
+    responses.add_callback(
+        responses.POST,
+        BASE_URL + "/subscriptions",
+        callback=request_callback,
+        content_type="application/json",
+    )
+
+    subscription = client.subscribe_to_event_changes(
+        "fake_calendar_id", webhook_url="https://example.com", secret="s3cr3t"
+    )
+
+    assert subscription["resource"] == "/me/calendars/fake_calendar_id/events"
+    assert subscription["changeType"] == "created,updated,deleted"
+    assert subscription["notificationUrl"] == "https://example.com"
+    assert subscription["clientState"] == "s3cr3t"
+
+
+subscriptions_json = {
+    "value": [
+        {
+            "id": "1ab7c0e6-8073-4121-ad7f-a477a30b6c25",
+            "resource": "/me/calendars",
+            "changeType": "updated,deleted",
+            "notificationUrl": "https://example.com/1",
+        },
+        {
+            "id": "39529cfc-ad79-4add-9a1d-14e4b4a4dfdb",
+            "resource": "/me/calendars",
+            "changeType": "updated,deleted",
+            "notificationUrl": "https://example.com/2",
+        },
+    ]
+}
+
+
+@responses.activate(registry=OrderedRegistry)
+def test_iter_subscriptions(client):
+    responses.get(
+        BASE_URL + "/subscriptions", json=subscriptions_json,
+    )
+
+    subscriptions = client.iter_subscriptions()
+
+    assert {subscription["notificationUrl"] for subscription in subscriptions} == {
+        "https://example.com/1",
+        "https://example.com/2",
+    }
+
+
+@responses.activate
+def test_unsubscribe(client):
+    responses.delete(
+        BASE_URL + "/subscriptions/fake_subscription_id", body="",
+    )
+
+    assert client.unsubscribe("fake_subscription_id") == {}

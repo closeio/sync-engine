e3fc17a13fd31702367a903c09a573f7cfd2e212
Squeaky <squeaky_pl@gmx.com>
2022-10-19 12:50:22 +0200

    convert_microsoft_timezone_to_olson, get_microsoft_tzinfo, parse_msgraph_datetime_tz_as_utc, dump_datetime_as_msgraph_datetime_tz

---
+++ inbox/events/microsoft/parse.py
@@ -0,0 +1,43 @@
+import datetime
+from typing import Dict, Union
+
+import ciso8601
+import pytz
+
+from inbox.events.timezones import windows_timezones
+
+TzInfo = Union["pytz._UTCclass", pytz.tzinfo.StaticTzInfo, pytz.tzinfo.DstTzInfo]
+
+
+def convert_microsoft_timezone_to_olson(timezone_id: str) -> str:
+    if timezone_id in windows_timezones:
+        timezone_id = windows_timezones[timezone_id]
+
+    return timezone_id
+
+
+def get_microsoft_tzinfo(timezone_id: str) -> TzInfo:
+    timezone_id = convert_microsoft_timezone_to_olson(timezone_id)
+
+    return pytz.timezone(timezone_id)
+
+
+MsGraphDateTimeTimeZone = Dict[str, str]
+
+
+def parse_msgraph_datetime_tz_as_utc(datetime_tz: MsGraphDateTimeTimeZone):
+    """"""
+    tzinfo = get_microsoft_tzinfo(datetime_tz["timeZone"])
+    dt = ciso8601.parse_datetime(datetime_tz["dateTime"])
+
+    return tzinfo.localize(dt).astimezone(pytz.UTC)
+
+
+def dump_datetime_as_msgraph_datetime_tz(
+    dt: datetime.datetime,
+) -> MsGraphDateTimeTimeZone:
+    assert dt.tzinfo == pytz.UTC
+    return {
+        "dateTime": dt.replace(tzinfo=None, microsecond=0).isoformat() + ".0000000",
+        "timeZone": "UTC",
+    }

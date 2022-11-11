fc50308e01c4e22303130a75aaac42de1a145b81
Paweł Piotr Przeradowski <showerproof86@gmail.com>
2022-11-11 11:41:36 +0100

    Microsoft: Calendar model remove old webhook columns (#411)

---
+++ migrations/versions/257_remove_calendar_old_webhook_columns.py
@@ -0,0 +1,29 @@
+"""Remove calendar old webhook columns
+
+Revision ID: 4af0d2f17967
+Revises: 93cc6f4ce113
+Create Date: 2022-11-10 12:21:52.306392
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "4af0d2f17967"
+down_revision = "93cc6f4ce113"
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import mysql
+
+
+def upgrade():
+    op.drop_column("calendar", "gpush_expiration")
+    op.drop_column("calendar", "gpush_last_ping")
+
+
+def downgrade():
+    op.add_column(
+        "calendar", sa.Column("gpush_last_ping", mysql.DATETIME(), nullable=True)
+    )
+    op.add_column(
+        "calendar", sa.Column("gpush_expiration", mysql.DATETIME(), nullable=True)
+    )

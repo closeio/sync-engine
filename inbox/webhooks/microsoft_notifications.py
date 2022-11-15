2ecb351e4303c6887ee40830016ad3a6684cf57a
Squeaky <squeaky_pl@gmx.com>
2022-11-15 12:02:22 +0100

    validate_webhook_payload

--- inbox/webhooks/microsoft_notifications.py
+++ inbox/webhooks/microsoft_notifications.py
@@ -1,6 +1,14 @@
 from functools import wraps
+from typing import List, cast
 
 from flask import Blueprint, make_response, request
+
+from inbox.config import config
+from inbox.events.microsoft.graph_types import (
+    MsGraphChangeNotification,
+    MsGraphChangeNotificationCollection,
+    MsGraphType,
+)
 
 app = Blueprint(
     "microsoft_webhooks", "microsoft_webhooks_api", url_prefix="/w/microsoft"
@@ -21,13 +29,45 @@ def handle_initial_validation_response(view_function):
     return _handle_initial_validation_response
 
 
+def validate_webhook_payload_factory(type: MsGraphType):
+    def validate_webhook_payload(view_function):
+        @wraps(view_function)
+        def _validate_webhook_payload(*args, **kwargs):
+            change_notifications: List[MsGraphChangeNotification] = cast(
+                MsGraphChangeNotificationCollection, request.json
+            )["value"]
+
+            if any(
+                notification["clientState"] != config["MICROSOFT_SUBSCRIPTION_SECRET"]
+                for notification in change_notifications
+            ):
+                return (
+                    "'clientState' did not match one provided when creating subscription",
+                    400,
+                )
+
+            if any(
+                notification["resourceData"]["@odata.type"] != type
+                for notification in change_notifications
+            ):
+                return f"Expected '@odata.type' to be '{type}'", 400
+
+            return view_function(*args, **kwargs)
+
+        return _validate_webhook_payload
+
+    return validate_webhook_payload
+
+
 @app.route("/calendar_list_update/<account_public_id>", methods=["POST"])
 @handle_initial_validation_response
+@validate_webhook_payload_factory("#Microsoft.Graph.Calendar")
 def calendar_update(account_public_id):
     return "calendar_list_update"
 
 
 @app.route("/calendar_update/<calendar_public_id>", methods=["POST"])
 @handle_initial_validation_response
+@validate_webhook_payload_factory("#Microsoft.Graph.Event")
 def event_update(calendar_public_id):
     return "calendar_list_update"
--- tests/webhooks/test_microsoft_notifications.py
+++ tests/webhooks/test_microsoft_notifications.py
@@ -1,9 +1,65 @@
+from unittest import mock
+
+import pytest
+
+from inbox.config import config
+
+
+@pytest.fixture(autouse=True)
+def populate_microsoft_subscrtipion_secret():
+    with mock.patch.dict(config, {"MICROSOFT_SUBSCRIPTION_SECRET": "good_s3cr3t"}):
+        yield
+
+
 def test_handle_initial_validation_response(test_client):
     response = test_client.post(
         "/w/microsoft/calendar_list_update/fake_id",
         query_string={"validationToken": "asd"},
     )
 
-    assert response.data == b"asd"
+    assert response.data.decode() == "asd"
     assert response.mimetype == "text/plain"
     assert response.status_code == 200
+
+
+bad_client_state_payload = {
+    "value": [
+        {
+            "changeType": "updated",
+            "resourceData": {
+                "@odata.type": "#Microsoft.Graph.Calendar",
+                "id": "fake_id",
+            },
+            "clientState": "wrong_s3cr3t",
+        }
+    ]
+}
+
+bad_type_payload = {
+    "value": [
+        {
+            "changeType": "updated",
+            "resourceData": {"@odata.type": "#Microsoft.Graph.Event", "id": "fake_id"},
+            "clientState": "good_s3cr3t",
+        }
+    ]
+}
+
+
+@pytest.mark.parametrize(
+    "payload,data",
+    [
+        (
+            bad_client_state_payload,
+            "'clientState' did not match one provided when creating subscription",
+        ),
+        (bad_type_payload, "Expected '@odata.type' to be '#Microsoft.Graph.Calendar'"),
+    ],
+)
+def test_validate_webhook_payload(test_client, payload, data):
+    response = test_client.post(
+        "/w/microsoft/calendar_list_update/fake_id", json=payload
+    )
+
+    assert response.data.decode() == data
+    assert response.status_code == 400

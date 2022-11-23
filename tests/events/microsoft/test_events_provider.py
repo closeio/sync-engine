7306b5b0ce15666b028deed411fe4bbaeedf555d
Squeaky <squeaky_pl@gmx.com>
2022-11-23 16:29:23 +0100

    Extract calendars_response, events_responses, subscribe_respones

--- tests/events/microsoft/test_events_provider.py
+++ tests/events/microsoft/test_events_provider.py
@@ -27,32 +27,223 @@ def populate_url_prefix():
 
 
 @pytest.fixture
-def provider(client):
-    provider = MicrosoftEventsProvider("fake_account_id", "fake_namespace_id")
-    provider.client = client
-
+def calendars_response():
     responses.get(
-        BASE_URL + "/me/calendars", json=calendars_json,
+        BASE_URL + "/me/calendars",
+        json={
+            "value": [
+                {
+                    "id": "fake_calendar_id",
+                    "name": "Calendar",
+                    "canEdit": True,
+                    "isDefaultCalendar": True,
+                },
+                {
+                    "id": "fake_test_calendar_id",
+                    "name": "Test",
+                    "canEdit": True,
+                    "isDefaultCalendar": False,
+                },
+            ],
+        },
     )
+
+
+@pytest.fixture
+def events_responses():
     responses.get(
-        BASE_URL + "/me/calendars/fake_calendar_id/events", json=events_json,
+        BASE_URL + "/me/calendars/fake_calendar_id/events",
+        json={
+            "value": [
+                {
+                    "@odata.etag": 'W/"bX+IPMmPs02YGYesdF/+dAAB/52fpA=="',
+                    "id": "singular_id",
+                    "createdDateTime": "2022-09-07T08:39:36.2273624Z",
+                    "lastModifiedDateTime": "2022-09-07T08:41:36.5027961Z",
+                    "changeKey": "bX+IPMmPs02YGYesdF/+dAAB/52fpA==",
+                    "categories": [],
+                    "transactionId": "962593bf-9e1b-ef34-bff6-da63d058df7f",
+                    "originalStartTimeZone": "Eastern Standard Time",
+                    "originalEndTimeZone": "Eastern Standard Time",
+                    "iCalUId": "040000008200E00074C5B7101A82E00800000000D0C4525C95C2D80100000000000000001000000007003FD5ECC09F42A0ACCA4299772507",
+                    "reminderMinutesBeforeStart": 15,
+                    "isReminderOn": True,
+                    "hasAttachments": False,
+                    "subject": "Singular",
+                    "bodyPreview": "",
+                    "importance": "normal",
+                    "sensitivity": "normal",
+                    "isAllDay": False,
+                    "isCancelled": False,
+                    "isOrganizer": True,
+                    "responseRequested": True,
+                    "seriesMasterId": None,
+                    "showAs": "busy",
+                    "type": "singleInstance",
+                    "onlineMeetingUrl": None,
+                    "isOnlineMeeting": True,
+                    "onlineMeetingProvider": "teamsForBusiness",
+                    "allowNewTimeProposals": True,
+                    "isDraft": False,
+                    "hideAttendees": False,
+                    "recurrence": None,
+                    "onlineMeeting": {
+                        "joinUrl": "https://teams.microsoft.com/l/meetup-join/xyz"
+                    },
+                    "responseStatus": {
+                        "response": "organizer",
+                        "time": "0001-01-01T00:00:00Z",
+                    },
+                    "body": {"contentType": "html", "content": "<i>Singular</i>"},
+                    "start": {
+                        "dateTime": "2022-09-15T12:00:00.0000000",
+                        "timeZone": "UTC",
+                    },
+                    "end": {
+                        "dateTime": "2022-09-15T12:30:00.0000000",
+                        "timeZone": "UTC",
+                    },
+                    "location": {
+                        "displayName": "Balcony",
+                        "locationType": "default",
+                        "uniqueIdType": "unknown",
+                        "address": {},
+                        "coordinates": {},
+                    },
+                    "locations": [],
+                    "attendees": [],
+                    "organizer": {
+                        "emailAddress": {
+                            "name": "Example <>",
+                            "address": "example_2@example.com",
+                        }
+                    },
+                },
+                {
+                    "@odata.etag": 'W/"bX+IPMmPs02YGYesdF/+dAACDYEM6g=="',
+                    "id": "recurrence_id",
+                    "createdDateTime": "2022-09-24T15:32:22.239054Z",
+                    "lastModifiedDateTime": "2022-09-27T14:41:23.1042764Z",
+                    "changeKey": "bX+IPMmPs02YGYesdF/+dAACDYEM6g==",
+                    "categories": [],
+                    "transactionId": "68faba75-324e-1e37-018d-b239fe0d3c8b",
+                    "originalStartTimeZone": "Pacific Standard Time",
+                    "originalEndTimeZone": "Pacific Standard Time",
+                    "iCalUId": "040000008200E00074C5B7101A82E00800000000F8620CD72AD0D801000000000000000010000000EB99E61264138D46A203CC0931BB688A",
+                    "reminderMinutesBeforeStart": 15,
+                    "isReminderOn": True,
+                    "hasAttachments": False,
+                    "subject": "Recurring",
+                    "bodyPreview": "",
+                    "importance": "normal",
+                    "sensitivity": "normal",
+                    "isAllDay": False,
+                    "isCancelled": False,
+                    "isOrganizer": True,
+                    "responseRequested": True,
+                    "seriesMasterId": None,
+                    "showAs": "busy",
+                    "type": "seriesMaster",
+                    "onlineMeetingUrl": None,
+                    "isOnlineMeeting": False,
+                    "onlineMeetingProvider": "unknown",
+                    "allowNewTimeProposals": True,
+                    "isDraft": False,
+                    "hideAttendees": False,
+                    "onlineMeeting": None,
+                    "responseStatus": {
+                        "response": "organizer",
+                        "time": "0001-01-01T00:00:00Z",
+                    },
+                    "body": {"contentType": "html", "content": "<b>Hello world!</b>"},
+                    "start": {
+                        "dateTime": "2022-09-19T15:00:00.0000000",
+                        "timeZone": "UTC",
+                    },
+                    "end": {
+                        "dateTime": "2022-09-19T15:30:00.0000000",
+                        "timeZone": "UTC",
+                    },
+                    "location": {
+                        "displayName": "Parking",
+                        "locationType": "default",
+                        "uniqueIdType": "unknown",
+                        "address": {},
+                        "coordinates": {},
+                    },
+                    "locations": [
+                        {
+                            "displayName": "Parking",
+                            "locationType": "default",
+                            "uniqueIdType": "unknown",
+                            "address": {},
+                            "coordinates": {},
+                        },
+                    ],
+                    "recurrence": {
+                        "pattern": {
+                            "type": "daily",
+                            "interval": 1,
+                            "month": 0,
+                            "dayOfMonth": 0,
+                            "firstDayOfWeek": "sunday",
+                            "index": "first",
+                        },
+                        "range": {
+                            "type": "endDate",
+                            "startDate": "2022-09-19",
+                            "endDate": "2022-09-21",
+                            "recurrenceTimeZone": "Pacific Standard Time",
+                            "numberOfOccurrences": 0,
+                        },
+                    },
+                    "attendees": [
+                        {
+                            "type": "required",
+                            "status": {
+                                "response": "declined",
+                                "time": "2022-09-08T15:40:17Z",
+                            },
+                            "emailAddress": {
+                                "name": "attendee@example.com",
+                                "address": "attendee@example.com",
+                            },
+                        }
+                    ],
+                    "organizer": {
+                        "emailAddress": {
+                            "name": "Example",
+                            "address": "example@example.com",
+                        }
+                    },
+                },
+            ]
+        },
     )
     responses.get(
         BASE_URL + "/me/calendars/fake_test_calendar_id/events", json={"value": []}
     )
-    responses.get(
-        BASE_URL + "/me/events/recurrence_id/instances", json=recurrence_instances_json
-    )
 
+
+@pytest.fixture
+def subscribe_responses():
     responses.post(
         BASE_URL + "/subscriptions",
-        json=calendar_list_subscribe_json,
+        json={
+            "id": "f798ca9d-d630-4306-b065-af52199f5613",
+            "resource": "/me/calendars",
+            "expirationDateTime": "2022-11-24T18:31:12.829451Z",
+        },
         match=[json_params_matcher({"resource": "/me/calendars"}, strict_match=False)],
     )
 
     responses.post(
         BASE_URL + "/subscriptions",
-        json=calendar_subscribe_json,
+        json={
+            "id": "f798ca9d-d630-4306-b065-af52199f5613",
+            "resource": "/me/calendars/fake_calendar_id/events",
+            "expirationDateTime": "2022-10-25T04:22:34.929451Z",
+        },
         match=[
             json_params_matcher(
                 {"resource": "/me/calendars/fake_calendar_id/events"},
@@ -63,7 +254,11 @@ def provider(client):
 
     responses.post(
         BASE_URL + "/subscriptions",
-        json=calendar_subscribe_json,
+        json={
+            "id": "f798ca9d-d630-4306-b065-af52199f5613",
+            "resource": "/me/calendars/fake_calendar_id/events",
+            "expirationDateTime": "2022-10-25T04:22:34.929451Z",
+        },
         match=[
             json_params_matcher(
                 {"resource": "/me/calendars/fake_test_calendar_id/events"},
@@ -72,28 +267,21 @@ def provider(client):
         ],
     )
 
+
+@pytest.fixture
+def provider(client):
+    provider = MicrosoftEventsProvider("fake_account_id", "fake_namespace_id")
+    provider.client = client
+
+    responses.get(
+        BASE_URL + "/me/events/recurrence_id/instances", json=recurrence_instances_json
+    )
+
     return provider
 
 
-calendars_json = {
-    "value": [
-        {
-            "id": "fake_calendar_id",
-            "name": "Calendar",
-            "canEdit": True,
-            "isDefaultCalendar": True,
-        },
-        {
-            "id": "fake_test_calendar_id",
-            "name": "Test",
-            "canEdit": True,
-            "isDefaultCalendar": False,
-        },
-    ],
-}
-
-
 @responses.activate
+@pytest.mark.usefixtures("calendars_response")
 def test_sync_calendars(provider):
     _, calendars = provider.sync_calendars()
     calendars_by_name = {calendar.name: calendar for calendar in calendars}
@@ -102,150 +290,6 @@ def test_sync_calendars(provider):
     assert calendars_by_name["Calendar"].default
     assert not calendars_by_name["Test"].read_only
     assert not calendars_by_name["Test"].default
-
-
-events_json = {
-    "value": [
-        {
-            "@odata.etag": 'W/"bX+IPMmPs02YGYesdF/+dAAB/52fpA=="',
-            "id": "singular_id",
-            "createdDateTime": "2022-09-07T08:39:36.2273624Z",
-            "lastModifiedDateTime": "2022-09-07T08:41:36.5027961Z",
-            "changeKey": "bX+IPMmPs02YGYesdF/+dAAB/52fpA==",
-            "categories": [],
-            "transactionId": "962593bf-9e1b-ef34-bff6-da63d058df7f",
-            "originalStartTimeZone": "Eastern Standard Time",
-            "originalEndTimeZone": "Eastern Standard Time",
-            "iCalUId": "040000008200E00074C5B7101A82E00800000000D0C4525C95C2D80100000000000000001000000007003FD5ECC09F42A0ACCA4299772507",
-            "reminderMinutesBeforeStart": 15,
-            "isReminderOn": True,
-            "hasAttachments": False,
-            "subject": "Singular",
-            "bodyPreview": "",
-            "importance": "normal",
-            "sensitivity": "normal",
-            "isAllDay": False,
-            "isCancelled": False,
-            "isOrganizer": True,
-            "responseRequested": True,
-            "seriesMasterId": None,
-            "showAs": "busy",
-            "type": "singleInstance",
-            "onlineMeetingUrl": None,
-            "isOnlineMeeting": True,
-            "onlineMeetingProvider": "teamsForBusiness",
-            "allowNewTimeProposals": True,
-            "isDraft": False,
-            "hideAttendees": False,
-            "recurrence": None,
-            "onlineMeeting": {
-                "joinUrl": "https://teams.microsoft.com/l/meetup-join/xyz"
-            },
-            "responseStatus": {"response": "organizer", "time": "0001-01-01T00:00:00Z"},
-            "body": {"contentType": "html", "content": "<i>Singular</i>"},
-            "start": {"dateTime": "2022-09-15T12:00:00.0000000", "timeZone": "UTC"},
-            "end": {"dateTime": "2022-09-15T12:30:00.0000000", "timeZone": "UTC"},
-            "location": {
-                "displayName": "Balcony",
-                "locationType": "default",
-                "uniqueIdType": "unknown",
-                "address": {},
-                "coordinates": {},
-            },
-            "locations": [],
-            "attendees": [],
-            "organizer": {
-                "emailAddress": {
-                    "name": "Example <>",
-                    "address": "example_2@example.com",
-                }
-            },
-        },
-        {
-            "@odata.etag": 'W/"bX+IPMmPs02YGYesdF/+dAACDYEM6g=="',
-            "id": "recurrence_id",
-            "createdDateTime": "2022-09-24T15:32:22.239054Z",
-            "lastModifiedDateTime": "2022-09-27T14:41:23.1042764Z",
-            "changeKey": "bX+IPMmPs02YGYesdF/+dAACDYEM6g==",
-            "categories": [],
-            "transactionId": "68faba75-324e-1e37-018d-b239fe0d3c8b",
-            "originalStartTimeZone": "Pacific Standard Time",
-            "originalEndTimeZone": "Pacific Standard Time",
-            "iCalUId": "040000008200E00074C5B7101A82E00800000000F8620CD72AD0D801000000000000000010000000EB99E61264138D46A203CC0931BB688A",
-            "reminderMinutesBeforeStart": 15,
-            "isReminderOn": True,
-            "hasAttachments": False,
-            "subject": "Recurring",
-            "bodyPreview": "",
-            "importance": "normal",
-            "sensitivity": "normal",
-            "isAllDay": False,
-            "isCancelled": False,
-            "isOrganizer": True,
-            "responseRequested": True,
-            "seriesMasterId": None,
-            "showAs": "busy",
-            "type": "seriesMaster",
-            "onlineMeetingUrl": None,
-            "isOnlineMeeting": False,
-            "onlineMeetingProvider": "unknown",
-            "allowNewTimeProposals": True,
-            "isDraft": False,
-            "hideAttendees": False,
-            "onlineMeeting": None,
-            "responseStatus": {"response": "organizer", "time": "0001-01-01T00:00:00Z"},
-            "body": {"contentType": "html", "content": "<b>Hello world!</b>"},
-            "start": {"dateTime": "2022-09-19T15:00:00.0000000", "timeZone": "UTC"},
-            "end": {"dateTime": "2022-09-19T15:30:00.0000000", "timeZone": "UTC"},
-            "location": {
-                "displayName": "Parking",
-                "locationType": "default",
-                "uniqueIdType": "unknown",
-                "address": {},
-                "coordinates": {},
-            },
-            "locations": [
-                {
-                    "displayName": "Parking",
-                    "locationType": "default",
-                    "uniqueIdType": "unknown",
-                    "address": {},
-                    "coordinates": {},
-                },
-            ],
-            "recurrence": {
-                "pattern": {
-                    "type": "daily",
-                    "interval": 1,
-                    "month": 0,
-                    "dayOfMonth": 0,
-                    "firstDayOfWeek": "sunday",
-                    "index": "first",
-                },
-                "range": {
-                    "type": "endDate",
-                    "startDate": "2022-09-19",
-                    "endDate": "2022-09-21",
-                    "recurrenceTimeZone": "Pacific Standard Time",
-                    "numberOfOccurrences": 0,
-                },
-            },
-            "attendees": [
-                {
-                    "type": "required",
-                    "status": {"response": "declined", "time": "2022-09-08T15:40:17Z"},
-                    "emailAddress": {
-                        "name": "attendee@example.com",
-                        "address": "attendee@example.com",
-                    },
-                }
-            ],
-            "organizer": {
-                "emailAddress": {"name": "Example", "address": "example@example.com",}
-            },
-        },
-    ]
-}
 
 
 recurrence_instances_json = {
@@ -267,6 +311,7 @@ recurrence_instances_json = {
 
 
 @responses.activate
+@pytest.mark.usefixtures("events_responses")
 def test_sync_events(provider):
     events = provider.sync_events("fake_calendar_id")
     events_by_title = {event.title: event for event in events}
@@ -277,27 +322,15 @@ def test_sync_events(provider):
     assert events_by_title["Recurring"].description == "Hello world!"
 
 
-calendar_list_subscribe_json = {
-    "id": "f798ca9d-d630-4306-b065-af52199f5613",
-    "resource": "/me/calendars",
-    "expirationDateTime": "2022-11-24T18:31:12.829451Z",
-}
-
-
 @responses.activate
+@pytest.mark.usefixtures("subscribe_responses")
 def test_watch_calendar_list(provider, outlook_account):
     expiration = provider.watch_calendar_list(outlook_account)
     assert expiration == datetime.datetime(2022, 11, 24, 18, 31, 12, tzinfo=pytz.UTC)
 
 
-calendar_subscribe_json = {
-    "id": "f798ca9d-d630-4306-b065-af52199f5613",
-    "resource": "/me/calendars/fake_calendar_id/events",
-    "expirationDateTime": "2022-10-25T04:22:34.929451Z",
-}
-
-
 @responses.activate
+@pytest.mark.usefixtures("subscribe_responses")
 def test_watch_calendar(provider, outlook_account):
     calendar = Calendar(uid="fake_calendar_id", public_id="fake_public_id")
 
@@ -310,6 +343,9 @@ def test_webhook_notifications_enabled(provider, outlook_account):
 
 
 @responses.activate
+@pytest.mark.usefixtures(
+    "calendars_response", "events_responses", "subscribe_responses"
+)
 def test_sync(db, provider, outlook_account):
     event_sync = WebhookEventSync(
         outlook_account.email_address,

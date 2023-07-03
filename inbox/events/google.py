d7ea4b1fab76d6c89d141f78725bff8c746b84d2
Squeaky <squeaky_pl@gmx.com>
2023-07-03 19:35:55 +0200

    type: ignore

--- inbox/events/google.py
+++ inbox/events/google.py
@@ -500,12 +500,12 @@ STRING_VALIDATORS = [
 
 @attrs.frozen(kw_only=True)
 class EntryPoint:
-    uri: str = attrs.field(validator=STRING_VALIDATORS)
+    uri: str = attrs.field(validator=STRING_VALIDATORS)  # type: ignore
 
 
 @attrs.frozen(kw_only=True)
 class ConferenceSolution:
-    name: str = attrs.field(validator=STRING_VALIDATORS)
+    name: str = attrs.field(validator=STRING_VALIDATORS)  # type: ignore
 
 
 @attrs.frozen(kw_only=True)

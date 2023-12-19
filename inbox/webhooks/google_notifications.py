c55640e23eb40d58fd0d31853ee449b9fb43b2f1
Squeaky <squeaky_pl@gmx.com>
2023-12-19 15:09:54 +0100

    Add throttle to /calendar_list_update/<account_public_id>

--- inbox/webhooks/google_notifications.py
+++ inbox/webhooks/google_notifications.py
@@ -63,14 +63,18 @@ def calendar_update(account_public_id):
     request.environ["log_context"]["account_public_id"] = account_public_id
     try:
         valid_public_id(account_public_id)
-        with global_session_scope() as db_session:
-            account = (
-                db_session.query(GmailAccount)
-                .filter(GmailAccount.public_id == account_public_id)
-                .one()
-            )
-            account.handle_webhook_notification()
-            db_session.commit()
+        allowed, tokens, sleep = limitlion.throttle(
+            f"gcal_account:{account_public_id}", rps=0.5
+        )
+        if allowed:
+            with global_session_scope() as db_session:
+                account = (
+                    db_session.query(GmailAccount)
+                    .filter(GmailAccount.public_id == account_public_id)
+                    .one()
+                )
+                account.handle_webhook_notification()
+                db_session.commit()
         return resp(200)
     except ValueError:
         raise InputError("Invalid public ID")
@@ -84,7 +88,7 @@ def event_update(calendar_public_id):
     try:
         valid_public_id(calendar_public_id)
         allowed, tokens, sleep = limitlion.throttle(
-            f"gcal:{calendar_public_id}", rps=0.5
+            f"gcal_calendar:{calendar_public_id}", rps=0.5
         )
         if allowed:
             with global_session_scope() as db_session:

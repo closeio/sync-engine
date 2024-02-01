9a89a296de5b21f20717097fdddba7f4239a91bc
Vyacheslav Tverskoy <tsx@tsx.su>
2024-02-01 06:29:40 +0000

    Fix imports in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,17 +11,10 @@ os.environ["NYLAS_ENV"] = "test"
 
 from pytest import fixture
 
-from inbox.util.testutils import (
-    dump_dns_queries,  # noqa
-    files,  # noqa
-    mock_dns_resolver,  # noqa
-    mock_imapclient,  # noqa
-    mock_smtp_get_connection,  # noqa
-    uploaded_file_ids,  # noqa
-)
-
 from tests.api.base import TestAPIClient
-from tests.util.base import *  # noqa
+
+# Additional fixtures
+pytest_plugins = ["inbox.util.testutils", "tests.util.base"]
 
 
 @fixture

c0281ec834b7a96a9209b0f4e1ffc8132c58fa20
Squeaky <squeaky_pl@gmx.com>
2024-06-24 14:55:41 +0200

    Remove pyinstrument

--- inbox/util/debug.py
+++ inbox/util/debug.py
@@ -1,40 +1,5 @@
 """Utilities for debugging failures in development/staging."""
 
-import signal
-from functools import wraps
-
-from pyinstrument import Profiler
-
-
-def profile(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        profiler = Profiler()
-        profiler.start()
-        r = func(*args, **kwargs)
-        profiler.stop()
-        print(profiler.output_text(color=True))
-        return r
-
-    return wrapper
-
-
-def attach_pyinstrument_profiler():
-    """Run the pyinstrument profiler in the background and dump its output to
-    stdout when the process receives SIGTRAP. In general, you probably want to
-    use the facilities in inbox.util.profiling instead.
-    """
-    profiler = Profiler()
-    profiler.start()
-
-    def handle_signal(signum, frame):
-        print(profiler.output_text(color=True))
-        # Work around an arguable bug in pyinstrument in which output gets
-        # frozen after the first call to profiler.output_text()
-        delattr(profiler, "_root_frame")
-
-    signal.signal(signal.SIGTRAP, handle_signal)
-
 
 def bind_context(gr, role, account_id, *args):
     """Bind a human-interpretable "context" to the greenlet `gr`, for

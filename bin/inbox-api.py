9633e92f1e8269feebc41597285180facd706fcd
Paweł Piotr Przeradowski <showerproof86@gmail.com>
2024-09-25 09:25:52 +0200

    Drop gevent in API pods #2 (#902)
    * Port API
    
    * Remove Tracer

--- bin/inbox-api.py
+++ bin/inbox-api.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-from gevent import monkey
-
-monkey.patch_all()
 
 import os
 import sys
--- inbox/api/wsgi.py
+++ inbox/api/wsgi.py
@@ -1,23 +1,16 @@
-import gevent.monkey
-
-gevent.monkey.patch_all()
-
 import logging
 import sys
 
 import json_log_formatter
-from gunicorn.workers.ggevent import GeventWorker
+from gunicorn.workers.gthread import ThreadWorker
 
-from inbox.instrumentation import Tracer
 from inbox.logging import configure_logging, get_logger
 
 log = get_logger()
 
 
-class NylasWSGIWorker(GeventWorker):
-    """Custom worker class for gunicorn. Based on
-    gunicorn.workers.ggevent.GeventPyWSGIWorker.
-    """
+class NylasWSGIWorker(ThreadWorker):
+    """Custom worker class for gunicorn."""
 
     def init_process(self):
         print("Python", sys.version, file=sys.stderr)
@@ -26,16 +19,14 @@ class NylasWSGIWorker(GeventWorker):
 
         configure_logging(log_level=LOGLEVEL)
 
-        if MAX_BLOCKING_TIME:
-            self.tracer = Tracer(max_blocking_time=MAX_BLOCKING_TIME)
-            self.tracer.start()
+        assert not config.get("USE_GEVENT", True), "USE_GEVENT must be False"
+
         super().init_process()
 
 
 from inbox.config import config
 from inbox.error_handling import maybe_enable_rollbar
 
-MAX_BLOCKING_TIME = config.get("MAX_BLOCKING_TIME", 1.0)
 LOGLEVEL = config.get("LOGLEVEL", 10)
 
 

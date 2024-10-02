f21fb7bec0f3c381b4fe76d6c19b98d44cd984ec
Squeaky <squeaky_pl@gmx.com>
2024-10-02 20:15:47 +0200

    Remove parts of greenlet-like we don't need

--- inbox/greenlet_like.py
+++ inbox/greenlet_like.py
@@ -1,21 +1,10 @@
-import dataclasses
 import queue
 import threading
-from typing import Any, Callable, Dict, Optional, Tuple, TypeVar
+from typing import Optional, TypeVar
 
 
 class GreenletLikeThreadExit(BaseException):
     pass
-
-
-@dataclasses.dataclass
-class GreenletLikeTarget:
-    target: Callable[..., Any]
-    args: Tuple[Any, ...]
-    kwargs: Dict[str, Any]
-
-    def __call__(self) -> Any:
-        return self.target(*self.args, **self.kwargs)
 
 
 CHECK_KILLED_TIMEOUT = 0.2
@@ -24,12 +13,9 @@ QueueElementT = TypeVar("QueueElementT")
 
 
 class GreenletLikeThread(threading.Thread):
-    def __init__(
-        self, target: Optional[Callable[..., Any]] = None, *args: Any, **kwargs: Any
-    ) -> None:
+    def __init__(self) -> None:
         self.__should_be_killed = False
         self.__ready = False
-        self.__run_target = GreenletLikeTarget(target, args, kwargs) if target else None
         self.exception: Optional[Exception] = None
 
         super().__init__()
@@ -51,10 +37,7 @@ class GreenletLikeThread(threading.Thread):
             self.__ready = True
 
     def _run(self) -> None:
-        if self.__run_target:
-            self.__run_target()
-        else:
-            raise NotImplementedError()
+        raise NotImplementedError()
 
     def kill(self, block: bool = True) -> None:
         self.__should_be_killed = True
@@ -71,12 +54,3 @@ class GreenletLikeThread(threading.Thread):
                 return queue.get(timeout=CHECK_KILLED_TIMEOUT)
             except queue.Empty:
                 self.check_killed()
-
-
-def spawn(
-    target: Optional[Callable[..., Any]] = None, *args: Any, **kwargs: Any
-) -> GreenletLikeThread:
-    thread = GreenletLikeThread(target, *args, **kwargs)
-    thread.start()
-
-    return thread

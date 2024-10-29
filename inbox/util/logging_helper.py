6eccadfaef978317d9c705a5c95ba2892eac13fd
Squeaky <squeaky_pl@gmx.com>
2024-10-29 15:59:31 +0100

    Tune logging for s3transfer

--- inbox/util/logging_helper.py
+++ inbox/util/logging_helper.py
@@ -4,6 +4,6 @@ import logging
 
 
 def reconfigure_logging():
-    logging.getLogger("boto").setLevel(logging.ERROR)
     logging.getLogger("boto3").setLevel(logging.ERROR)
     logging.getLogger("botocore").setLevel(logging.ERROR)
+    logging.getLogger("s3transfer").setLevel(logging.ERROR)
